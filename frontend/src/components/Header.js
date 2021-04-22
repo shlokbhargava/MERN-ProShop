246101745fba439a98d4c415b4770fdf945f2b30
Shlok Bhargava <shlokbhargava@MacBook-Air.local>
2021-04-23 02:24:56 +0530

    Header footer

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Container, Nav, Navbar } from 'react-bootstrap'
+
+const Header = () => {
+    return (
+        <header>
+            <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+                <Container>
+                    <Navbar.Brand href="/">ProShop</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto">
+                        <Nav.Link href="/cart"><i class="fas fa-shopping-cart"></i> Cart</Nav.Link>
+                        <Nav.Link href="/login"><i class="fas fa-sign-in-alt"></i> Sign In</Nav.Link> 
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    )
+}
+
+export default Header
