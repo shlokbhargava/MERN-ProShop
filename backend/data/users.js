5275744e3e38fd1bdf925e942b57db5f7e2ae087
Shlok Bhargava <shlokbhargava@MacBook-Air.local>
2021-04-24 17:42:21 +0530

    sample users

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,18 +1,22 @@
+import bcrypt from 'bcryptjs'
+
 const users = [
     {
         name: 'Admin',
         email: 'admin@example.com',
-        password: xxxx,
+        password: bcrypt.hashSync('admin1', 10),
         isAdmin: true,
     },
     {
         name: 'John Doe',
         email: 'john@example.com',
-        password: xxxx,
+        password: bcrypt.hashSync('123', 10),
     },
     {
         name: 'Jane Doe',
         email: 'jane@example.com',
-        password: xxxx,
+        password: bcrypt.hashSync('1234', 10),
     }
 ]
+
+export default users
