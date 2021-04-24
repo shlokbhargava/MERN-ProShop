1a4315c5178359ad48065f61be65cfc73da0f4f7
Shlok Bhargava <shlokbhargava@MacBook-Air.local>
2021-04-24 19:31:42 +0530

    Data seeder script

---
+++ backend/seeder.js
@@ -0,0 +1,61 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import products from './data/products.js'
+import users from './data/users.js'
+import User from './models/userModel.js'
+import Order from './models/orderModel.js'
+import Product from './models/productModel.js'
+
+dotenv.config()
+
+connectDB()
+
+
+const importData = async () => {
+    
+    try {
+        await User.deleteMany()
+        await Product.deleteMany()
+        await Order.deleteMany()
+
+        const createdUsers = await User.insertMany(users)
+
+        const adminUser = createdUsers[0]._id
+        
+        const sampleProducts = products.map(product => {
+            return{ ...product, user: adminUser }
+        })
+
+        await Product.insertMany(sampleProducts)
+
+        console.log('Data imported!')
+        process.exit()
+    } catch (error) {
+        console.error(error)
+        process.exit(1)
+    }
+}
+
+
+const destroyData = async () => {
+
+    try {
+        await User.deleteMany()
+        await Product.deleteMany()
+        await Order.deleteMany()
+
+        console.log('Data destroyed!')
+        process.exit()
+    } catch (error) {
+        console.error(error)
+        process.exit(1)
+    }
+}
+
+
+if (process.argv[2] === '-d') {
+    destroyData()
+} else {
+    importData()
+}
