514ede741cdedd7585524c8b22e2b39124a8fa13
Shlok Bhargava <shlokbhargava@MacBook-Air.local>
2021-04-30 02:18:31 +0530

    user authentication

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import { notFound, errorHandler } from './middlewares/errorMiddleware.js'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
@@ -11,12 +12,15 @@ connectDB()
 
 const app = express()
 
+// body parser
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('API is running')
 })
 
 app.use('/api/products', productRoutes)
-
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 
