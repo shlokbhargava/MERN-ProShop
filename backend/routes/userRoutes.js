4dca48b975404131f1e074d47e0b692ca353dcf4
Shlok Bhargava <shlokbhargava@MacBook-Air.local>
2021-05-01 00:20:01 +0530

    user authorisation using jwt and registering user

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,11 @@
 import express from 'express'
-import { authUser } from '../controllers/userController.js'
+import { authUser, getUserProfile, createUser } from '../controllers/userController.js'
+import { protect } from '../middlewares/authMiddleware.js'
 
 const router = express.Router()
 
+router.post('/', createUser)
 router.post('/login', authUser)
+router.get('/profile', protect, getUserProfile)
 
 export default router
